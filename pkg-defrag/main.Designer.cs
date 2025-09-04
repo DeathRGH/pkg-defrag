e9fcc7ad5dec5ed804984b30afa4cf26d2e96797
DeathRGH <sebihanel@gmail.com>
2025-09-04 02:27:53 +0200

    Add project files.

---
+++ pkg-defrag/main.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace pkg_defrag
+{
+    partial class main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			btn_selectImage = new Button();
+			box_imageOffset = new TextBox();
+			label_imageOffset = new Label();
+			label_chunkSize = new Label();
+			box_chunkSize = new TextBox();
+			label_padSize = new Label();
+			box_padding = new TextBox();
+			btn_startExtract = new Button();
+			label_imageName = new Label();
+			SuspendLayout();
+			// 
+			// btn_selectImage
+			// 
+			btn_selectImage.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			btn_selectImage.Location = new Point(12, 12);
+			btn_selectImage.Name = "btn_selectImage";
+			btn_selectImage.Size = new Size(300, 23);
+			btn_selectImage.TabIndex = 0;
+			btn_selectImage.Text = "Select Image...";
+			btn_selectImage.UseVisualStyleBackColor = true;
+			btn_selectImage.Click += btn_selectImage_Click;
+			// 
+			// box_imageOffset
+			// 
+			box_imageOffset.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			box_imageOffset.Location = new Point(12, 120);
+			box_imageOffset.Name = "box_imageOffset";
+			box_imageOffset.Size = new Size(300, 23);
+			box_imageOffset.TabIndex = 1;
+			box_imageOffset.Text = "0x44D5FB8000";
+			// 
+			// label_imageOffset
+			// 
+			label_imageOffset.AutoSize = true;
+			label_imageOffset.Location = new Point(12, 102);
+			label_imageOffset.Name = "label_imageOffset";
+			label_imageOffset.Size = new Size(137, 15);
+			label_imageOffset.TabIndex = 2;
+			label_imageOffset.Text = "Image Offset (PKG Start):";
+			// 
+			// label_chunkSize
+			// 
+			label_chunkSize.AutoSize = true;
+			label_chunkSize.Location = new Point(12, 146);
+			label_chunkSize.Name = "label_chunkSize";
+			label_chunkSize.Size = new Size(68, 15);
+			label_chunkSize.TabIndex = 4;
+			label_chunkSize.Text = "Chunk Size:";
+			// 
+			// box_chunkSize
+			// 
+			box_chunkSize.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			box_chunkSize.Location = new Point(12, 164);
+			box_chunkSize.Name = "box_chunkSize";
+			box_chunkSize.Size = new Size(300, 23);
+			box_chunkSize.TabIndex = 3;
+			box_chunkSize.Text = "0x28000000";
+			// 
+			// label_padSize
+			// 
+			label_padSize.AutoSize = true;
+			label_padSize.Location = new Point(12, 190);
+			label_padSize.Name = "label_padSize";
+			label_padSize.Size = new Size(149, 15);
+			label_padSize.TabIndex = 6;
+			label_padSize.Text = "Padding Size (data to skip):";
+			// 
+			// box_padding
+			// 
+			box_padding.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			box_padding.Location = new Point(12, 208);
+			box_padding.Name = "box_padding";
+			box_padding.Size = new Size(300, 23);
+			box_padding.TabIndex = 5;
+			box_padding.Text = "0x6400000";
+			// 
+			// btn_startExtract
+			// 
+			btn_startExtract.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			btn_startExtract.Location = new Point(12, 269);
+			btn_startExtract.Name = "btn_startExtract";
+			btn_startExtract.Size = new Size(300, 23);
+			btn_startExtract.TabIndex = 7;
+			btn_startExtract.Text = "Extract";
+			btn_startExtract.UseVisualStyleBackColor = true;
+			btn_startExtract.Click += btn_startExtract_Click;
+			// 
+			// label_imageName
+			// 
+			label_imageName.AutoSize = true;
+			label_imageName.Location = new Point(12, 38);
+			label_imageName.Name = "label_imageName";
+			label_imageName.Size = new Size(108, 15);
+			label_imageName.TabIndex = 8;
+			label_imageName.Text = "No image selected!";
+			// 
+			// main
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(324, 308);
+			Controls.Add(label_imageName);
+			Controls.Add(btn_startExtract);
+			Controls.Add(label_padSize);
+			Controls.Add(box_padding);
+			Controls.Add(label_chunkSize);
+			Controls.Add(box_chunkSize);
+			Controls.Add(label_imageOffset);
+			Controls.Add(box_imageOffset);
+			Controls.Add(btn_selectImage);
+			Name = "main";
+			Text = "PKG Defrag";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button btn_selectImage;
+		private TextBox box_imageOffset;
+		private Label label_imageOffset;
+		private Label label_chunkSize;
+		private TextBox box_chunkSize;
+		private Label label_padSize;
+		private TextBox box_padding;
+		private Button btn_startExtract;
+		private Label label_imageName;
+	}
+}
---
+++ pkg-defrag/main.cs
@@ -0,0 +1,105 @@
+using System.Text;
+
+namespace pkg_defrag
+{
+	public partial class main : Form
+	{
+		string? imagePath = null;
+
+		public main()
+		{
+			InitializeComponent();
+		}
+
+		private void btn_selectImage_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				imagePath = ofd.FileName;
+				label_imageName.Text = ofd.SafeFileName;
+			}
+		}
+
+		private void btn_startExtract_Click(object sender, EventArgs e)
+		{
+			if (imagePath == null)
+			{
+				MessageBox.Show("Select a image first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			long imgOffset = Convert.ToInt64(box_imageOffset.Text, 16);
+
+			using FileStream imgStream = new FileStream(imagePath, FileMode.Open);
+
+			// read pkg name
+			imgStream.Seek(imgOffset, SeekOrigin.Begin);
+			imgStream.Seek(0x40, SeekOrigin.Current);
+			byte[] nameBytes = new byte[0x24];
+			imgStream.Read(nameBytes, 0, 0x24);
+			string pkgName = Encoding.ASCII.GetString(nameBytes);
+
+			// read pkg size
+			long pkgFileSize = -1;
+			imgStream.Seek(imgOffset, SeekOrigin.Begin);
+			imgStream.Seek(0x430, SeekOrigin.Current);
+			byte[] fileSizeBytes = new byte[8];
+			imgStream.Read(fileSizeBytes, 0, 8);
+			Array.Reverse(fileSizeBytes);
+			pkgFileSize = BitConverter.ToInt64(fileSizeBytes, 0);
+
+			if (pkgFileSize == -1)
+			{
+				MessageBox.Show("Select offset does not point to a pkg file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			if (MessageBox.Show($"Do you want to extract {pkgName} with file size 0x{pkgFileSize:X}?", "Extract", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+			{
+				return;
+			}
+
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.FileName = $"{pkgName}.pkg";
+			if (sfd.ShowDialog() == DialogResult.OK)
+			{
+				long bytesLeft = pkgFileSize;
+				int copyBufferSize = 0;
+
+				int chunkSize = Convert.ToInt32(box_chunkSize.Text, 16);
+				int padding = Convert.ToInt32(box_padding.Text, 16);
+
+				byte[] copyBuffer = new byte[chunkSize];
+
+				using FileStream pkgStream = new FileStream(sfd.FileName, FileMode.Create);
+
+				imgStream.Seek(imgOffset, SeekOrigin.Begin);
+
+				while (bytesLeft > 0)
+				{
+					if (bytesLeft > chunkSize)
+					{
+						copyBufferSize = chunkSize;
+						bytesLeft -= chunkSize;
+					}
+					else
+					{
+						copyBufferSize = (int)bytesLeft;
+						bytesLeft -= bytesLeft;
+					}
+
+					// copy full chunk size from image to our new pkg file
+					imgStream.Read(copyBuffer, 0, copyBufferSize);
+
+					pkgStream.Write(copyBuffer, 0, copyBufferSize);
+
+					// advance only the image stream by the padding
+					imgStream.Seek(padding, SeekOrigin.Current);
+				}
+			}
+
+			MessageBox.Show("Done");
+		}
+	}
+}
